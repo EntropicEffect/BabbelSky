bb0378eb407e119cceb8935645ea3e73e05a8856
William Grunow <william.grunow93@gmail.com>
2024-12-03 15:07:37 -0500

    Update ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,28 +9,29 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-export default [...compat.extends(
+export default [
+  ...compat.extends(
     "eslint:recommended",
     "plugin:mozilla/recommended",
     "plugin:prettier/recommended",
-), {
+  ),
+  {
     plugins: {
-        mozilla,
-        prettier,
+      mozilla,
+      prettier,
     },
 
     languageOptions: {
-        globals: {
-            ...globals.browser,
-            chrome: "readonly",
-        },
+      globals: {
+        ...globals.browser,
+        chrome: "readonly",
+      },
     },
-
     rules: {},
-    ignores: ["eslint.config.mjs"],
-}];
+  },
+];
