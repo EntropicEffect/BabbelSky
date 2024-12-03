4f8e427595ad3c899729ebadac1dd97f7893c582
William Grunow <william.grunow93@gmail.com>
2024-12-03 14:31:33 -0500

    Update ESLint config to new format

---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+import mozilla from "eslint-plugin-mozilla";
+import prettier from "eslint-plugin-prettier";
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [...compat.extends(
+    "eslint:recommended",
+    "plugin:mozilla/recommended",
+    "plugin:prettier/recommended",
+), {
+    plugins: {
+        mozilla,
+        prettier,
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+            chrome: "readonly",
+        },
+    },
+
+    rules: {},
+}];
